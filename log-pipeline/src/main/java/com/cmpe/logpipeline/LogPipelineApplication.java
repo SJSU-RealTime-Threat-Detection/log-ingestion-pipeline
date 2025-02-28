0a7c43db527a3899b123d3fddc8120ae304d6607
Miyar Karthik Kamath <miyarkarthikkamath@gmail.com>
2025-02-28 04:22:49 -0800

    Initial commit with project structure and basic setup

---
+++ log-pipeline/src/main/java/com/cmpe/logpipeline/LogPipelineApplication.java
@@ -0,0 +1,13 @@
+package com.cmpe.logpipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogPipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogPipelineApplication.class, args);
+	}
+
+}
